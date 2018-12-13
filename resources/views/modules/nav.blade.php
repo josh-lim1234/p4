b70ba109cbd24d9d47a47c5b29cca42297f7b4c0
josh-lim1234 <joshlim1234@gmail.com>
2018-12-13 23:26:58 +0800

    no extra scroll

--- resources/views/modules/nav.blade.php
+++ resources/views/modules/nav.blade.php
@@ -1,63 +1,55 @@
 <header class="header-area">
-    <!-- Top Header Area -->
-    <div class="top-header-area bg-img bg-overlay" style="background-image: url(/img/bg-img/header.jpg);">
-        <div class="container h-100">
-            <div class="row h-100 align-items-center justify-content-between">
-                <div class="col-12 col-sm-12">
-                    <h3>FooooooooooD</h3>
+
+        <!-- Top Header Area -->
+        <div class="top-header-area bg-img bg-overlay" style="background-image: url(/img/bg-img/header.jpg);">
+        </div>
+
+        <!-- Logo Area -->
+        <div class="logo-area">
+            <a href="/photos"><img src="/img/core-img/logo.png" alt=""></a>
+        </div>
+
+        <!-- Navbar Area -->
+<div class="bueno-main-menu" id="sticker">
+    <div class="classy-nav-container breakpoint-off">
+        <div class="container">
+            <!-- Menu -->
+            <nav class="classy-navbar justify-content-between" id="buenoNav">
+                <!-- Menu -->
+                <div class="classy-menu">
+
+                    <!-- Close Button -->
+                    <div class="classycloseIcon">
+                        <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                    </div>
+
+                    <!-- Nav Start -->
+                    <div class="classynav">
+                        <ul>
+                            <li><a href="/photos">Home</a>
+                            </li>
+                    @if(Auth::check())
+                        <li><a href="/photos/create">New Post</a></li>
+                        <li>
+                            {{ Auth::user()->name }}
+                            <ul class="dropdown">
+                                <li>
+                                    <form method='POST' id='logout' action='/logout'>
+                                        {{ csrf_field() }}
+                                        <a href='#' onClick='document.getElementById("logout").submit();'>
+                                         Logout</a>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <a href="/login">Login</a>
+                    @endif                         
+                        </ul>
+                    </div>
+                        </div>
+                    </nav>
                 </div>
             </div>
         </div>
-    </div>
-
-    <!-- Navbar Area -->
-    <div class="bueno-main-menu" id="sticker">
-        <div class="classy-nav-container breakpoint-off">
-            <div class="container">
-                <!-- Menu -->
-                <nav class="classy-navbar justify-content-between" id="buenoNav">          
-                        <div id="toggler">                         
-                        </div>
-                        <!-- Navbar Toggler -->
-                        <div class="classy-navbar-toggler">
-                            <span class="navbarToggler"><span></span><span></span><span></span></span>
-                        </div>
-                    <!-- Menu -->
-                    <div class="classy-menu">
-
-                        <!-- Close Button -->
-                        <div class="classycloseIcon">
-                            <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                        </div>
-
-                        <!-- Nav Start -->
-                        <div class="classynav">
-                            <ul>
-                                <li><a href="/photos">Home</a></li>
-                            @if(Auth::check())
-                                <li><a href="/photos/create">New Post</a></li>
-                                <li>
-                                    {{ Auth::user()->name }}
-                                    <ul class="dropdown">
-                                        <li>
-                                            <form method='POST' id='logout' action='/logout'>
-                                                {{ csrf_field() }}
-                                                <a href='#' onClick='document.getElementById("logout").submit();'>
-                                                 Logout</a>
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </li>
-                            @else
-                <a href="/login">Login</a>            
-                            @endif
-
-                            </ul>
-                        </div>
-                        <!-- Nav End -->
-                    </div>
-                </nav>
-            </div>
-        </div>
-    </div>
-</header>
+    </header>
